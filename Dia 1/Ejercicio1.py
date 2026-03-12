0f3e08b2c6af2c04305abd965fa51cd2b948f5e6
Jorge Arce <jorgearcedev1@gmail.com>
2026-03-11 21:05:42 -0700

    Ejercicio dia 1

---
+++ Dia 1/Ejercicio1.py
@@ -0,0 +1,5 @@
+print("Escribe el nombre de tu cancion favorita")
+print("Tu cancion favorita es: " + input("Escribe el nombre de tu cancion favorita: "))
+
+print("Escribe una parte de esa cancion")
+print("La parte de la cancion que te gusta es: " + input("Escribe una parte de esa cancion: "))
---
+++ Dia 1/input.py
@@ -0,0 +1,4 @@
+print(input("Escribe tu nombre: "))
+print(input("Escribe tu edad: "))
+
+print("Tu altura es de: " + input("Escribe tu altura: "))
