b931778d14cbf92928292bd3f2a169bc6cdf56b4
Jorge Arce <jorgearcedev1@gmail.com>
2026-03-12 15:00:34 -0700

    Dia 2

---
+++ Dia 2/int_float.py
@@ -0,0 +1,5 @@
+mi_numero = 1
+print(mi_numero)
+
+edad = input("¿Cuántos años tienes? ")
+print("Tu edad es de: " + edad)
---
+++ Dia 2/variables.py
@@ -0,0 +1,9 @@
+nombre = "Jorge"
+edad = 22
+altura = 1.78
+print(nombre)
+print(edad)
+print(altura)
+
+preguntarpeso = input("¿Cuánto pesas? ")
+print("Tu peso es de: " + preguntarpeso)
